91d3ea238f76e08cf3c5764c29070796550a06f6
vovaf88 <vovaf88@mail.ru>
2024-02-27 22:11:30 +0300

    add models Bank, Operation, BankAccount

--- office/models.py
+++ office/models.py
@@ -53,3 +53,50 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+class Bank(models.Model):
+    bik = models.CharField(max_length=9)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class BankAccount(models.Model):
+    number = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+
+class PartnerBankAccount(BankAccount):
+    bank = models.ForeignKey(Bank, blank=True, null=True, on_delete=models.SET_NULL, related_name='partner_bank_acc')
+    owner = models.ForeignKey(Partner, blank=True, null=True, on_delete=models.SET_NULL, related_name='partner_bank_acc')
+
+    def __str__(self):
+        return f'{self.owner} - {self.number}'
+
+
+class MyBankAccount(BankAccount):
+    bank = models.ForeignKey(Bank, blank=True, null=True, on_delete=models.SET_NULL, related_name='my_bank_acc')
+    owner = models.ForeignKey(MyCompany, blank=True, null=True, on_delete=models.SET_NULL, related_name='my_bank_acc')
+
+    def __str__(self):
+        return f'{self.owner} - {self.number}'
+
+
+class Operation(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Doc(models.Model):
+    number = models.CharField(max_length=7)
+    doc_date = models.DateTimeField()
+    operation = models.ForeignKey(Operation, on_delete=models.SET_NULL, blank=True, null=True, related_name='doc')
+    my_company = models.ForeignKey(MyCompany, on_delete=models.CASCADE, related_name='doc')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f'{self.operation} номер {self.number} от {self.doc_date}'
